b4150ec045e917f39606698571a50a2a27f85100
Lemoon Can <lemoon.can@gmail.com>
2024-02-04 16:59:09 +0800

    refactor: 💡 清理无关代码

--- _js/app.js
+++ _js/app.js
@@ -8,21 +8,7 @@ $(document).ready(function() {
   });
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('landing', 'assets/particles.json', function() {});
-
-  // Typing Text
-  var element = document.getElementById('txt-rotate');
-  var toRotate = element.getAttribute('data-rotate');
-  var period = element.getAttribute('data-period');
-  setTimeout(function() {
-    new TxtRotate(element, JSON.parse(toRotate), period);
-  }, 1500);
-
-  // INJECT CSS
-  var css = document.createElement('style');
-  css.type = 'text/css';
-  css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
-  document.body.appendChild(css);
+  // particlesJS.load('landing', 'assets/particles.json', function() {});
 
   // Initialize AOS
   AOS.init({
@@ -34,7 +20,7 @@ $(document).ready(function() {
     once: true
   });
 
-  randomizeOrder();
+  // randomizeOrder();
 });
 
 /* FUNCTIONS */
@@ -62,62 +48,4 @@ function fadeOutPreloader(element, duration) {
       element.style.filter = 'alpha(opacity = ' + opacity * 100 + ')';
     }
   }, duration);
-}
-
-/* Typing Text */
-
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 5;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-/* Word Cloud */
-
-function randomizeOrder() {
-  var parent = document.getElementById('skills');
-  var divs = parent.getElementsByTagName('div');
-  var frag = document.createDocumentFragment();
-
-  // Randomize order of skills
-  while (divs.length) {
-    frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
-  }
-  parent.appendChild(frag);
 }
